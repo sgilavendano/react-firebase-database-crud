fb3a9745c3dbceb35c6f5f832d1487f74208fcb7
sgi <69767184+sgilavendano@users.noreply.github.com>
2021-06-22 21:14:44 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,16 @@ import * as firebase from "firebase";
 import "firebase/database";
 
 let config = {
-  apiKey: "xxx",
-  authDomain: "bezkoder-firebase.firebaseapp.com",
-  databaseURL: "https://bezkoder-firebase.firebaseio.com",
-  projectId: "bezkoder-firebase",
-  storageBucket: "bezkoder-firebase.appspot.com",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+    apiKey: "AIzaSyDwaQgvZwjdkJfw7KOD59MUFzQhsjYq-oU",
+    authDomain: "yardsale-42be8.firebaseapp.com",
+    databaseURL: "https://yardsale-42be8.firebaseio.com",
+    projectId: "yardsale-42be8",
+    storageBucket: "yardsale-42be8.appspot.com",
+    messagingSenderId: "414562145420",
+    appId: "1:414562145420:web :db3f1016a7a5c8fbff97a9",
+
+    //I cant find this anywhereee
+    measurementId: "G-XW79NYCRE4",
 };
 
 firebase.initializeApp(config);
